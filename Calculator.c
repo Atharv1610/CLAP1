e0eee6a69c741c25373469308f814b0c02bf083c
Atharv1610 <103433059+Atharv1610@users.noreply.github.com>
2022-04-28 10:46:40 +0530

    Update and rename Addition_Multiplication.c to Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -7,7 +7,7 @@ printf(“Enter first number \n”);
 scanf(“%d”,&a);
 printf(”Enter second number\n”);
 scanf(“%d”,&b);
-printf(“Enter 1 for addition or 2 for multiplication\n”);
+printf(“Enter 1 for addition or 2 for multiplication or 3 for subtraction and 4 for division\n”);
 scanf(“%d”,&choice);
 if(choice==1)
 result=a+b;
@@ -15,6 +15,12 @@ printf("The addition of the no. is =%d",result);
 else if(choice==2)
 result=a*b;
 printf("The multiplication of the two numbers is =%d",result);
+else if(choice==3)
+result=a-b;
+printf("The subtraction of the two numbers is =%d",result);
+else if(choice==4)
+result=a/b;
+printf("The division of the two numbers is =%d",result);
 
 return 0;
 
